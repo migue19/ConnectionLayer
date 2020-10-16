f0cb10df4b43de82028cbb789551d65b74b58b28
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2020-10-16 12:31:36 -0500

    Initial Commit

---
+++ ConnectionLayer/ConnectionLayer.h
@@ -0,0 +1,18 @@
+//
+//  ConnectionLayer.h
+//  ConnectionLayer
+//
+//  Created by Miguel Mexicano Herrera on 16/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConnectionLayer.
+FOUNDATION_EXPORT double ConnectionLayerVersionNumber;
+
+//! Project version string for ConnectionLayer.
+FOUNDATION_EXPORT const unsigned char ConnectionLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConnectionLayer/PublicHeader.h>
+
+
